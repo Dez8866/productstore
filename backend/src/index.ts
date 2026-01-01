0e06862ccc86bef7cb69d1b68594ab704e4eb88e
Dez8866 <sai.amar7749@gmail.com>
2026-01-01 17:56:05 +0000

    feat: update dependencies and add comment, product, and user controllers with routes
    - Updated @clerk/express to version 1.7.60
    - Downgraded drizzle-kit to version 0.18.1
    - Added commentController for creating and deleting comments
    - Added productController for CRUD operations on products
    - Added userController for syncing user data
    - Created routes for comments, products, and users
    - Integrated new routes into the main application
    - Added global type definitions for Clerk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,11 @@ import cors from "cors"
 import { clerkMiddleware } from '@clerk/express'
 
 
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
+
+
 
 const app = express()
 
@@ -23,5 +28,11 @@ app.get("/", (req, res) => {
         }
     });
 });
+
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
+
 
 app.listen(ENV.PORT, () => console.log("server is up and running on PORT", ENV.PORT))
